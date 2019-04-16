7f2a360b5c577a8aa1b30f6f699cdbbf2d1194c9
Pick83 <dominik.strebel@gmail.com>
2019-04-16 10:10:22 +0200

    Hostname changed

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,7 @@
 #include <boost/serialization/serialization.hpp>
 #include <boost/serialization/string.hpp>
 #include <boost/serialization/vector.hpp>
+#include <unistd.h>
 
 constexpr int MASTER = 0;
 constexpr int TAG_RESULT = 1;
@@ -33,18 +34,23 @@ constexpr int TAG_FILE = 10;
 int main(int argc, char **argv)
 {
   //Initialize global object
-  boost::mpi::environment env{argc, argv};
+  char hostname[255]{};
+  boost::mpi::environment env{argc, argv, false};
   boost::mpi::communicator world;
-  std::vector<Result> results;
   Config runningConf;
   std::map<int, std::string> inv_vertice_map;
   std::map<std::string, int> vertice_map;
   std::vector<std::string> keys;
   std::vector<int> vals;
+  std::vector<Result> results;
   std::vector<std::string> radius_file_list;
   std::deque<std::string> chain_file_list;
+  Result *res_vec;
+  auto rank = world.rank();
+  auto world_size = world.size();
+  gethostname(hostname, 255);
 
-  if (world.rank() == MASTER)
+  if (rank == MASTER)
   {
     //MASTER CODE
     BOOST_LOG_TRIVIAL(info) << "****************************************";
@@ -53,6 +59,7 @@ int main(int argc, char **argv)
     BOOST_LOG_TRIVIAL(info) << "Calculates different properties";
     BOOST_LOG_TRIVIAL(info) << "by Dominik Strebel (2019)";
     BOOST_LOG_TRIVIAL(info) << "****************************************";
+    BOOST_LOG_TRIVIAL(info) << "Hostname MASTER: " << hostname;
     try
     {
       runningConf = getCL(argc, argv);
@@ -71,7 +78,14 @@ int main(int argc, char **argv)
     chainpattern = trim(chainpattern);
     chain_file_list = glob_deq(chainpattern);
     radius_file_list = glob(xyzpattern);
+    if (radius_file_list.empty())
+    {
+      BOOST_LOG_TRIVIAL(info) << "Need at least one file with radii *.tet\n";
+      world.abort(-255);
+      exit(EXIT_FAILURE);
+    }
     auto radius_file = read_file(radius_file_list[0]);
+    
     radius_file.pop_back();
     std::sort(radius_file.begin(), radius_file.end(), cmp_radii);
     auto lookup_table = get_lookup_table(radius_file);
@@ -85,37 +99,31 @@ int main(int argc, char **argv)
   broadcast(world, keys, MASTER);
   broadcast(world, vals, MASTER);
 
-  if (world.rank() == MASTER)
+  if (rank == MASTER)
   {
     // sanity check of file list
-
-    if (radius_file_list.empty())
-    {
-      BOOST_LOG_TRIVIAL(info) << "Need at least one file with radii *.tet\n";
-      exit(EXIT_FAILURE);
-    }
     SI::natural::sort(chain_file_list);
-
     auto t_len = chain_file_list.size();
+    BOOST_LOG_TRIVIAL(info) << "t_len = " << t_len;
+    results.resize(chain_file_list.size());
     //Check that processes are <= length of tasks
-    if (world.size() > t_len + 1)
+    if (world_size > t_len + 1)
     {
-      BOOST_LOG_TRIVIAL(error) << "Too many processes (" << world.size()
+      BOOST_LOG_TRIVIAL(error) << "Too many processes (" << world_size
                                << ") for the number of jobs!\n";
       BOOST_LOG_TRIVIAL(error) << "Use " << t_len + 1 << " ranks or less\n";
       return 0;
     }
     long v_index = 0;
-    results.resize(t_len);
-    BOOST_LOG_TRIVIAL(info) << "Memory: " << sizeof(results) + sizeof(Result) * results.capacity();
-    std::vector<boost::mpi::request> reqs(world.size());
+    BOOST_LOG_TRIVIAL(info) << "Memory: " << results.size();
+    std::vector<boost::mpi::request> reqs(world_size);
 
-    for (unsigned int dst_rank = 1; dst_rank < world.size(); ++dst_rank)
+    for (int dst_rank = 1; dst_rank < world_size; ++dst_rank)
     {
       std::string file{chain_file_list.front()};
-     BOOST_LOG_TRIVIAL(info) << "[MASTER] Sending job "
+      BOOST_LOG_TRIVIAL(info) << "[MASTER] Sending job "
                               << file << " to SLAVE (first loop) " << dst_rank << "\n";
-
+      BOOST_LOG_TRIVIAL(info) << "[MASTER] v_index = " << v_index;
       world.send(dst_rank, TAG_FILE, file.data(), file.size());
       chain_file_list.pop_front();
       // Post receive request for new jobs requests by slave [nonblocking]
@@ -125,7 +133,7 @@ int main(int argc, char **argv)
     while (chain_file_list.size() > 0)
     {
       bool stop;
-      for (unsigned int dst_rank = 1; dst_rank < world.size(); ++dst_rank)
+      for (int dst_rank = 1; dst_rank < world_size; ++dst_rank)
       {
         // Check if dst_rank is done
         if (reqs[dst_rank].test())
@@ -137,13 +145,14 @@ int main(int argc, char **argv)
             // Tell the slave that a new job is coming.
             stop = false;
             world.send(dst_rank, TAG_BREAK, stop);
-            
-            // Send the new job.
-            std::string file{ chain_file_list.front()};
-            BOOST_LOG_TRIVIAL(info) << "[MASTER] Sending new job ("
-                                    << file << ") to SLAVE " << dst_rank << ".\n";
 
-            world.send(dst_rank, TAG_FILE,  file.data(), file.size());
+            // Send the new job.
+            std::string file{chain_file_list.front()};
+            BOOST_LOG_TRIVIAL(info) << "[MASTER] Sending new job ("
+                                    << file << ") to SLAVE " << dst_rank;
+             BOOST_LOG_TRIVIAL(info) << "[MASTER] v_index = " << v_index;
+
+            world.send(dst_rank, TAG_FILE, file.data(), file.size());
             chain_file_list.pop_front();
             reqs[dst_rank] = world.irecv(dst_rank, TAG_RESULT, results[v_index++]);
           }
@@ -164,7 +173,7 @@ int main(int argc, char **argv)
     while (!all_done)
     {
       all_done = true;
-      for (unsigned int dst_rank = 1; dst_rank < world.size(); ++dst_rank)
+      for (int dst_rank = 1; dst_rank < world_size; ++dst_rank)
       {
         if (reqs[dst_rank].test())
         {
@@ -182,49 +191,52 @@ int main(int argc, char **argv)
     BOOST_LOG_TRIVIAL(info) << "[MASTER] Handled all jobs, killed every process.\n";
   }
   // SLAVE CODE
-  if (world.rank() != MASTER)
+  if (rank > MASTER)
   {
     //Generate Map from string and int vector
-    std::map<std::string, int> v_map = constructMap(keys,vals);
+    std::map<std::string, int> v_map = constructMap(keys, vals);
     bool stop = false;
     while (!stop)
     {
       auto status = world.probe(0, TAG_FILE);
       auto nbytes = status.count<char>();
-      char *f_recv = new char[nbytes.get()+1] {};
-      BOOST_LOG_TRIVIAL(info) << "Initialized Job";
+      char *f_recv = new char[nbytes.get() + 1]{};
+      BOOST_LOG_TRIVIAL(info) << "[SLAVE: " << rank
+                              << "] (" << hostname << ") Intialized JOB";
       world.recv(0, TAG_FILE, f_recv, nbytes.get());
-      BOOST_LOG_TRIVIAL(info) << "Recevied " << nbytes.get() << " bytes";
+      BOOST_LOG_TRIVIAL(info) << "[SLAVE: " << rank
+                              << "] (" << hostname << ") Recevied: " 
+                              << nbytes.get() << " bytes";
       std::string file(f_recv);
       delete[] f_recv;
       BOOST_LOG_TRIVIAL(info) << file;
 
-      BOOST_LOG_TRIVIAL(info) << "[SLAVE: " << world.rank()
-                              << "] Received job "
+      BOOST_LOG_TRIVIAL(info) << "[SLAVE: " << rank
+                              << "] (" << hostname << ") Received job "
                               << file << " from MASTER.\n";
       // Perform "job"
-      BOOST_LOG_TRIVIAL(info) << "[SLAVE: " << world.rank()
-                              << "] Start Job\n";
+      BOOST_LOG_TRIVIAL(info) << "[SLAVE: " << rank
+                              << "] (" << hostname << ") Start Job\n";
       Graph mygraph(file, v_map);
       mygraph.calc();
       auto res = mygraph.get_result();
       // Send result
-      BOOST_LOG_TRIVIAL(info) << "[SLAVE: " << world.rank()
-                              << "] Done with job "
+      BOOST_LOG_TRIVIAL(info) << "[SLAVE: " << rank
+                              << "] (" << hostname << ") Done with job "
                               << file << ". Send Result.\n";
       world.send(0, TAG_RESULT, res);
       // Check if a new job is coming
       world.recv(0, TAG_BREAK, stop);
     }
 
-    BOOST_LOG_TRIVIAL(info) << "Rank " << world.rank() << " is exiting\n";
+    BOOST_LOG_TRIVIAL(info) << "Rank " << rank << " is exiting\n";
   }
-  if (world.rank() == MASTER)
-  {
+  if (rank == MASTER){
     std::sort(results.begin(), results.end(), cmp_ts);
     std::ofstream ts_mean_file(runningConf.OutputPath + "/properties.csv");
     write_ts_header(ts_mean_file, runningConf);
     ts_mean_file << std::setprecision(std::numeric_limits<double>::digits10 + 1);
+   
     for (auto &v : results)
     {
       ts_mean_file << std::to_string(v.ts) << runningConf.sep << std::to_string(v.mean)
