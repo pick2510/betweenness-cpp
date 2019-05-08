7cc2d360ceb7877a78726c0654c9c983a6e18419
Pick83 <dominik.strebel@gmail.com>
2019-05-08 11:44:25 +0200

    Parallel regrid

--- src/regrid.cpp
+++ src/regrid.cpp
@@ -58,32 +58,69 @@ int main(int argc, char **argv)
   Config runningConf = getGridConfigObj(reader);
   LogConfig(runningConf);
   const Decomposition decomp(runningConf);
-  const std::string path{runningConf.OutputPath + "/" +
-                         runningConf.contact_filename};
+  const std::string con_path{runningConf.OutputPath + "/" +
+                             runningConf.contact_filename};
+  const std::string part_path{runningConf.OutputPath + "/" +
+                              runningConf.particle_filename};
   std::map<int, double> radius_map;
-  auto stor = initTSStorage(path);
-  auto particles = indexContactStorage(path);
-  particles.sync_schema();
-  auto radius_storage = initRadstorage(path);
+  auto ts_stor = initTSStorage(con_path);
+  auto part_stor = ParticleIndexStorage(part_path);
+  auto con_stor = indexContactStorage(con_path);
+  auto radius_storage = initRadstorage(con_path);
   for (auto &elem : radius_storage.iterate<radius>()) {
     radius_map[elem.id] = elem.rad;
   }
-  auto ts_list = stor.get_all<ts_column>();
-  for (auto const ts : ts_list) {
-    auto selected_column = particles.get_all<ContactColumns>(
-        where(c(&ContactColumns::ts) == ts.ts));
-    particles.begin_transaction();
-    for (auto &elem : selected_column) {
-      auto coord = dumpfile::calc_contactpoint(elem, radius_map);
-      auto cell = decomp.calc_cell_numeric(coord);
-      elem.cell_x = cell.x;
-      elem.cell_y = cell.y;
-      elem.cell_z = cell.z;
-      elem.cellstr = decomp.calc_cell(coord);
-      particles.update(elem);
+  auto ts_list = ts_stor.get_all<ts_column>();
+#if defined(_OPENMP)
+#pragma omp parallel
+#pragma omp single
+  {
+#pragma omp task
+    {
+#endif
+      for (const auto ts : ts_list) {
+        BOOST_LOG_TRIVIAL(info) << "Contact ts: " << ts.ts;
+        auto selected_column = con_stor.get_all<ContactColumns>(
+            where(c(&ContactColumns::ts) == ts.ts));
+        con_stor.begin_transaction();
+        for (auto &elem : selected_column) {
+          auto coord = dumpfile::calc_contactpoint(elem, radius_map);
+          auto cell = decomp.calc_cell_numeric(coord);
+          elem.cell_x = cell.x;
+          elem.cell_y = cell.y;
+          elem.cell_z = cell.z;
+          elem.cellstr = decomp.calc_cell(coord);
+          con_stor.update(elem);
+        }
+        con_stor.commit();
+        // particles.update(selected_column);
+      }
+#if defined(_OPENMP)
     }
-    particles.commit();
-    // particles.update(selected_column);
+#pragma omp task
+    {
+#endif
+      for (const auto ts : ts_list) {
+        BOOST_LOG_TRIVIAL(info) << "Particle ts: " << ts.ts;
+        auto selected_column = part_stor.get_all<ParticleColumns>(
+            where(c(&ParticleColumns::ts) == ts.ts));
+        part_stor.begin_transaction();
+        for (auto &elem : selected_column) {
+          auto coord = coordinate{.x = elem.p_x, .y = elem.p_y, .z = elem.p_z};
+          auto cell = decomp.calc_cell_numeric(coord);
+          elem.cell_x = cell.x;
+          elem.cell_y = cell.y;
+          elem.cell_z = cell.z;
+          elem.cellstr = decomp.calc_cell(coord);
+          part_stor.update(elem);
+        }
+        part_stor.commit();
+        // particles.update(selected_column);
+      }
+
+#if defined(_OPENMP)
+    }
   }
+#endif
   return EXIT_SUCCESS;
 }
